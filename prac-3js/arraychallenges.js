bf9092179b3b5659e0e47d6a6d51de87ea182054
AbQ12 <abubakar.q098@gmail.com>
2025-11-18 12:47:36 +0500

    add .gitignore file to exclude unnecessary files from version control

--- prac-3js/arraychallenges.js
+++ prac-3js/arraychallenges.js
@@ -9,3 +9,43 @@ let firsttea = teaflvour[0];
 
 /* declare an aray named cities containing london ...
  access third element in the array and sotre it in a variable fav city */
+
+ let cities = ["london" , "paris" , "new york" , "berlin"];
+let favCity = cities[2];
+// console.log(favCity);
+
+
+/* you have an array named teatypescontaining herbeltea......
+        change the secind element of the array to jasmine tea */
+ 
+let teaTypes = ["herbal tea" , "white tea" , "masala chai" , "oolong tea"]
+teaTypes[1] = "jasmine tea";
+// console.log(teaTypes);
+
+
+/* declare an array name citites visited containing tokyo...and sydney
+     add berlin too array using push method */
+
+     let citiesvisited = ["tokyo" , "seoul" , "dubai" , "sydney"];
+     citiesvisited[citiesvisited.length] = "berlin"; // or you can use this
+    //  console.log(citiesvisited.length);
+     citiesvisited.push("hehe");
+    //  console.log(citiesvisited);
+
+/* 
+    you have an array names tea orders with chai ...
+     remove the last element of the array using the pop method and store it 
+     in a variable names last order */
+
+    let teaOrders = ["chai" , "matcha" , "herbal" , "earl grey"];
+    const lastorder = teaOrders.pop();
+
+    /* you have an array named popular teas containing green teaOrders...
+     create a soft copy of this array named  softcopies */
+
+     let popularTea = ["green tea" , "chai" , "oolong" , "black tea"];
+     let softcopies = popularTea;
+
+
+      
+     
