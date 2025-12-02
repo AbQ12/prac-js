abd33eecbadef0982177643a0eec465f947e6ede
AbQ12 <abubakar.q098@gmail.com>
2025-12-02 17:58:56 +0500

    done with bom and dom

---
+++ part4/dom.js
@@ -0,0 +1,48 @@
+//document object model
+//example 1
+document.getElementById('changeTextButton').
+addEventListener('click',function () {
+      let paragraph = document.getElementById('myParagraph');
+        paragraph.textContent = 'the para has benn changed';
+
+})
+
+//example 2
+
+document.getElementById('highlightFirstCity').
+addEventListener('click',function () {
+    let citieslist = document.getElementById('cityList');
+    citieslist.firstElementChild.classList.add('highlight');
+    
+})
+
+//example 3
+
+document.getElementById('changeOrder').
+addEventListener('click',function () {
+
+   let ct = document.getElementById('coffeeType');
+   ct.textContent = 'espresso';
+   ct.style.backgroundColor = 'lightblue';
+})
+
+// example 4
+
+document.getElementById('addItemButton').
+addEventListener('click',function () {
+     let newElement = document.createElement('li');
+     newElement.textContent = 'eegs';
+    document.getElementById('shoppingList').appendChild(newElement);
+
+
+})
+
+// example 5
+
+document.getElementById('removeLastTask').
+addEventListener('click',function () {
+  
+    let tasklst = document.getElementById('taskList');
+    tasklst.lastElementChild.remove();
+
+})
