59962a205747563aa5cd46575b4eba7e80994a25
AbQ12 <abubakar.q098@gmail.com>
2025-12-03 12:29:30 +0500

    time and loops

---
+++ explore/explore1.js
@@ -0,0 +1,14 @@
+console.log("abq");
+
+function greet(){
+    console.log('hello for my siede');
+    
+}
+setTimeout(() => {
+    greet();
+}, 4000);
+
+for (let index = 0; index < 10; index++) {
+    console.log(index);
+}
+        
